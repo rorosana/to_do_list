05a7da653656d37bae5cd497fa727b5cc4223863
rorosana <r.a.caraballo8@gmail.com>
2022-08-18 01:37:02 +0200

    added date function in app.js doc and index.html doc

---
+++ app.js
@@ -0,0 +1,19 @@
+//Info date
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+//Task container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es',{ day: 'numeric'});
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long'});
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short'});
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric'});
+};
+
+setDate();
+
